e844a89ae52ea0e0f8457e5c7a61db70531d7863
Teo <teodor.g.stefanov@mobiltel.bg>
2020-06-13 20:24:11 +0300

    added stylecop have to come with rules now

--- server/Models/Book.cs
+++ server/Models/Book.cs
@@ -1,16 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace server.Models
 {
     public class Book
     {
         public string Title { get; set; }
+
         public int AuthorId { get; set; }
+
         public Author Author { get; set; }
+
         public int Pages { get; set; }
+
         public DateTime YearOfPublish { get; set; }
     }
 }
