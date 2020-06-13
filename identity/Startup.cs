7e93c2df9849c4f6e7ff804bb07a42beead30fe2
Teo <teodor.g.stefanov@mobiltel.bg>
2020-06-13 18:10:06 +0300

    using watmore identity project with some changes

---
+++ identity/Startup.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApi.Helpers;
+using WebApi.Services;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+using System;
+using Microsoft.OpenApi.Models;
+using System.Linq;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers()
+                    .AddJsonOptions(x => x.JsonSerializerOptions.IgnoreNullValues = true);
+            services.AddSwaggerGen(c =>
+             {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "identity api", Version = "v1" });
+             });
+            // configure strongly typed settings objects
+            services.AddDbContext<DataContext>(options =>
+                         options.UseSqlServer(Configuration["DefaultConnection:connectionString"]));
+            
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+            var appSettings = appSettingsSection.Get<AppSettings>();
+              
+            // configure jwt authentication
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+            services.AddScoped<IUserService, UserService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext context)
+        {
+            // add hardcoded test user to db on startup
+            // plain text password is used for simplicity, hashed passwords should be used in production applications
+            if(!context.Users.Any()){
+                context.Users.Add(new User { FirstName = "Test", LastName = "User", Username = "test", Password = "test" });
+                context.SaveChanges();
+            }
+            app.UseRouting();
+            app.UseStaticFiles();
+            // global cors policy
+            app.UseCors(x => x
+                .SetIsOriginAllowed(origin => true)
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                 c.RoutePrefix = string.Empty;
+            });
+            app.UseEndpoints(x => x.MapControllers());
+        }
+    }
+}
